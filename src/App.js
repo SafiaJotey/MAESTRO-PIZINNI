94105fce3b15bef28155f8d8724de58cf1707686
Safia Jotey <safiaahmed1786@gmail.com>
2022-08-02 10:01:56 +0600

    add about us section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AboutUs from './components/AboutUS/AboutUs';
 import HeroSection from './components/HeroSection/HeroSection';
 
 function App() {
   return (
     <div className="App">
       <HeroSection></HeroSection>
+      <AboutUs />
     </div>
   );
 }
--- src/components/AboutUS/AboutUs.jsx
+++ src/components/AboutUS/AboutUs.jsx
@@ -3,9 +3,23 @@ import './AboutUs.css';
 
 const AboutUs = () => {
   return (
-    <div className="about">
-      <div className="about-content"></div>
-      <div className="about-image"></div>
+    <div className="container">
+      <div className=" about">
+        <div className="about-content">
+          <h5>About Us</h5>
+          <h4>WELCOME TO </h4>
+          <h4>MAESTRO PIZZINI</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            sequi ipsum reprehenderit aliquam maxime rem consequatur earum
+            ratione?
+          </p>
+          <button>READ MORE</button>
+        </div>
+        <div className="about-image">
+          <img src="/images/pizza.jpg" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
